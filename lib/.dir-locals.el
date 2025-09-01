ffd450268b187cd5ae2a78a7888018da8f77da38
Darren Embry <dse@webonastick.com>
2025-08-31 23:57:32 -0400

    for emacs

---
+++ lib/.dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((python-mode . ((fill-column . 128)
+                 (comment-column . 64))))
