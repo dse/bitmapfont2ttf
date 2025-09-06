e47f82e090084f909f4f28ad70507e197feda97f
Darren Embry <dse@webonastick.com>
2025-09-06 02:41:51 -0400

    not in use

--- lib/parse_bdf_utils.py
+++
@@ -1,8 +0,0 @@
-import os, sys
-if __name__ == "__main__":
-    sys.path.insert(0, os.path.dirname(__file__))
-
-def ellipsis(str):
-    if len(str) > 16:
-        return str[0:13] + "..."
-    return str
