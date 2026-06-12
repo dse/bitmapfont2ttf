3773754a7f5b80526f7fba8d09dd37e321cc00d7
Darren Embry <dse@webonastick.com>
2026-06-11 21:13:25 -0400

    rewrite sanity checks

--- lib/bdf_char.py
+++ lib/bdf_char.py
@@ -416,33 +416,37 @@ class BDFChar:
             self.encoding = -1
         self.char_name_variant_encoding_finalized = True
 
-    def sanity_check():
-        sane = True
+    #==========================================================================
+
+    def sanity_check(self):
+        self.sanity_check_swidth_dwidth_x()
+        self.sanity_check_encoding()
+        self.sanity_check_bitmap_data()
+
+    def sanity_check_swidth_dwidth_x(self):
         if self.swidth_x is not None and self.dwidth_x is not None:
             swidth_x_from_dwidth_x = round(self.dwidth_x / self.get_resolution_x() * 72.27 / self.get_point_size() * 1000)
             dwidth_x_from_swidth_x = round(self.swidth_x / 1000 * self.get_point_size() / 72.27 * self.get_resolution_x())
             if dwidth_x_from_swidth_x != self.dwidth_x:
-                print("WARNING: char %s: DWIDTH is %d; computed from SWIDTH is %d" % (self.name, self.dwidth_x, dwidth_x_from_swidth_x))
-                sane = False
+                print("WARNING: %s: char %s: DWIDTH is %d; computed from SWIDTH is %d" % (self.font.filename, self.name, self.dwidth_x, dwidth_x_from_swidth_x))
             if get_ratio(swidth_x_from_dwidth_x, self.swidth_x) > 1.01:
-                print("WARNING: char %s: SWIDTH is %d; computed from DWIDTH is %d" % (self.name, self.swidth_x, swidth_x_from_dwidth_x))
-                sane = False
+                print("WARNING: %s: char %s: SWIDTH is %d; computed from DWIDTH is %d" % (self.font.filename, self.name, self.swidth_x, swidth_x_from_dwidth_x))
+
+    def sanity_check_encoding(self):
         encoding_from_name = fontforge.unicodeFromName(self.name)
         if encoding_from_name >= 0:
             if self.encoding != encoding_from_name:
-                print("WARNING: char %s: encoding for that charname should be %d but ENCODING is %d" % (self.name, encoding_from_name, self.encoding))
-                sane = False
+                print("WARNING: %s: char %s: encoding for that charname should be %d but ENCODING is %d" % (self.font.filename, self.name, encoding_from_name, self.encoding))
             else:
                 canonical_name = fontforge.nameFromUnicode(self.encoding)
                 if self.name != canonical_name:
-                    print("NOTICE: char %s: canonical glyph name for ENCODING %d is %s" % (self.name, self.encoding, canonical_name))
+                    print("NOTICE: %s: char %s: canonical glyph name for ENCODING %d is %s" % (self.font.filename, self.name, self.encoding, canonical_name))
+
+    def sanity_check_bitmap_data(self):
         if len(self.bitmap_data) == 0:
-            print("WARNING: char %s: no bitmap data" % self.name)
-            sane = False
+            print("WARNING: %s: char %s: no bitmap data" % (self.font.filename, self.name))
         elif len(self.bitmap_data) != self.get_bbx_y():
-            print("WARNING: char %s: bounding box is %d pixels tall but bitmap has %d lines of data" % (self.name, self.get_bbx_y(), len(self.bitmap_data)))
-            sane = False
-        return sane
+            print("WARNING: %s: char %s: bounding box is %d pixels tall but bitmap has %d lines of data" % (self.font.filename, self.name, self.get_bbx_y(), len(self.bitmap_data)))
 
 unknown_charname_counter = 0
 unknown_variant_counter = 0
